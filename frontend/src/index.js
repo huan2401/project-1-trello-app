90f333bb87458b8275909e63686e47705847582e
DESKTOP-77TOL4L\Huan2401 <huannv.bhsoft@gmail.com>
2022-04-08 17:30:08 +0700

    init project frontend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,15 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// import {I18nextProvider} from 'react-i18next';
+import I18Provider from 'providers/I18Provider';
+// import i18n from 'i18n';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+    <React.StrictMode>
+        <I18Provider>
+            <App />
+        </I18Provider>
+    </React.StrictMode>
 );
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//       <I18Provider>
+//           <App />
+//       </I18Provider>
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
