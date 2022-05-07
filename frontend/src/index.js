a6710bfa0249cc50d06c321fbd77e185e89db2d6
DESKTOP-77TOL4L\Huan2401 <huannv.bhsoft@gmail.com>
2022-05-07 08:46:42 +0700

    FE/setup custom antd

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,31 +1,20 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-import React from "react";
-import ReactDOM from "react-dom/client";
-import './index.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.less';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-// import {I18nextProvider} from 'react-i18next';
+import {I18nextProvider} from 'react-i18next';
 import I18Provider from 'providers/I18Provider';
-// import i18n from 'i18n';
+import i18n from 'i18n';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-    <React.StrictMode>
-        <I18Provider>
-            <App />
-        </I18Provider>
-    </React.StrictMode>
+ReactDOM.render(
+  <React.StrictMode>
+      <I18Provider>
+          <App />
+      </I18Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
-
-// ReactDOM.render(
-//   <React.StrictMode>
-//       <I18Provider>
-//           <App />
-//       </I18Provider>
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
