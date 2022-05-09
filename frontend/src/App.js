8fdcac2c4f61684275b451219f235dcd4879280f
minhtq13 <taminh596@gmail.com>
2022-05-09 20:58:26 +0700

    Login and Signup form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,24 @@
-import './App.less';
-import {BrowserRouter as Router,Switch,Route} from 'react-router-dom';
+import "./App.less";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "containers/Home";
 import PrivateRoute from "components/common/PrivateRoute";
 import Login from "components/common/Login";
+import Signup from "components/common/Signup";
 
 function App() {
-  return (
-    <Router>
-      <Switch>
-        <Route path="/login">
-          <Login/>
-        </Route>
-        <PrivateRoute path="/" Component={Home}/>
-      </Switch>
-    </Router>
-  );
+    return (
+        <Router>
+            <Switch>
+                <Route path="/login">
+                    <Login />
+                </Route>
+                <Route path="/signup">
+                    <Signup />
+                </Route>
+                <PrivateRoute path="/" Component={Home} />
+            </Switch>
+        </Router>
+    );
 }
 
 export default App;
--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -1,18 +1,29 @@
-import { lazy } from 'react';
+import { lazy } from "react";
 
-const Home = lazy(() => import('containers/Home'));
-
+const Home = lazy(() => import("containers/Home"));
+// const Login = lazy(() => import("../components/common/Login/Login.js"));
+// const Signup = lazy(() => import("../components/common/Signup/Signup"));
 
 /*
  * If route has children, it's a parent menu (not link to any pages)
  * You can change permissions to your IAM's permissions
  */
-const routes= [
-  {
-    path: '/',
-    title: 'Home',
-    component: Home,
-  },
+const routes = [
+    {
+        path: "/login",
+        title: "Login",
+        component: Login,
+    },
+    // {
+    //     path: "/signup",
+    //     title: "Sign Up",
+    //     component: Signup,
+    // },
+    // {
+    //     path: "/",
+    //     title: "Home",
+    //     component: Home,
+    // },
 ];
 
 export default routes;
