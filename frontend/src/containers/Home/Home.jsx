4262a5b0d87c46dff10e319bdf19f63889d7dbb1
huan2401 <huannv.bhsoft@gmail.com>
2022-06-23 09:20:33 +0700

    setup

--- frontend/src/containers/Home/Home.jsx
+++ frontend/src/containers/Home/Home.jsx
@@ -1,12 +1,13 @@
-import React from 'react';
+import React, { useState } from "react";
+import { HomeWrapper } from "./CustomStyled";
 
 const Home = () => {
 
   return (
-    <>
-      Home
-    </>
-  )
-}
+    <HomeWrapper>
+      
+    </HomeWrapper>
+  );
+};
 
 export default Home;
